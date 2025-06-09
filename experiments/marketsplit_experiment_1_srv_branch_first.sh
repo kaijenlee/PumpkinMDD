018d4c739024222255bb2c923dcc47eccbea6ee7
kaijenlee <leekaijen@student.tudelft.nl>
2025-06-09 16:31:36 +0200

    Add scripts for qcp and market split branch srv first

---
+++ experiments/marketsplit_experiment_1_srv_branch_first.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define variables
+INPUT_DIR="benchmark_suite/market_split"   # Replace with your directory path
+OUTPUT_DIR="experiments/experiment_1/market_split_SRV_Branch_first" # Replace with your desired output file
+MODEL="$INPUT_DIR/market_split.mzn"
+# Create the output directory if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+# Loop through all files in the input directory
+for INPUT_FILE in "$INPUT_DIR"/*.dzn; do
+  if [ -f "$INPUT_FILE" ]; then
+    BASENAME=$(basename "$INPUT_FILE")             # Extract filename
+    OUTPUT_FILE="$OUTPUT_DIR/$BASENAME.txt"        # Define output file path (customize extension if needed)
+
+    echo "Processing $INPUT_FILE -> $OUTPUT_FILE"
+
+    # Perform your operation (Y) here. Replace 'cat' with your actual command.
+    minizinc --time-limit 1200000 --solver "minizinc/pumpkin.msc" $MODEL $INPUT_FILE -s --dd-enable --dd-srv-enable --branch-srv-first > $OUTPUT_FILE
+
+  fi
+done
+
+echo "All files processed. Outputs saved to $OUTPUT_DIR"
+python3 "experiments/extract_fzn_stats.py" $OUTPUT_DIR "experiments/experiment_1/marketsplit_srv_branch_first.csv"
---
+++ experiments/qpc_experiment_1_srv_branch_first.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define variables
+INPUT_DIR="benchmark_suite/QCP"   # Replace with your directory path
+OUTPUT_DIR="experiments/experiment_1/QCP_SRV_Branch_first" # Replace with your desired output file
+
+# Create the output directory if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+# Loop through all files in the input directory
+for INPUT_FILE in "$INPUT_DIR"/*.mzn; do
+  if [ -f "$INPUT_FILE" ]; then
+    BASENAME=$(basename "$INPUT_FILE")             # Extract filename
+    OUTPUT_FILE="$OUTPUT_DIR/$BASENAME.txt"        # Define output file path (customize extension if needed)
+
+    echo "Processing $INPUT_FILE -> $OUTPUT_FILE"
+
+    # Perform your operation (Y) here. Replace 'cat' with your actual command.
+    minizinc --time-limit 1200000 --solver "minizinc/pumpkin.msc" $INPUT_FILE -s --dd-enable --dd-srv-enable --branch-srv-first> $OUTPUT_FILE
+
+  fi
+done
+
+echo "All files processed. Outputs saved to $OUTPUT_DIR"
+python3 "experiments/extract_fzn_stats.py" $OUTPUT_DIR "experiments/experiment_1/qcp_srv_branch_first.csv"
